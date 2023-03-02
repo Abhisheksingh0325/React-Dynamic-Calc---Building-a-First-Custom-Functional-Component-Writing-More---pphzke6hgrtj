6f6cf472b8d1ae73a397355a5f793d00cfa66d42
Abhishek Singh <93363416+Abhisheksingh0325@users.noreply.github.com>
2023-03-03 00:18:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,19 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
-
 const App = () => {
-  const [expense, setExpense] = useState(0);
-  const handle = () => {
-    const a = document.createElement("li");
-    const b = document.getElementById("expense-input").value;
-    a.innerHTML = b;
-    document.getElementById("expense-list").appendChild(a);
-    const c = b.indexOf('-');  
-    const d = b.slice(c + 1).trim();
-    setExpense(parseInt(d) + expense);
-  }
+  const [firstNum, setFirstNum] = useState(0);
+  const [secondNum, setSecondNum] = useState(0);
+  
+  const handleChange1 =(event)=> {setFirstNum(event.target.value)}
+  const handleChange2 =(event)=> {setSecondNum(event.target.value)}
 
   return (
     <div id="main">
-      <input id="expense-input" />
-      <button id="expense-button" onClick={handle}>Add Expense</button>
-      <div id="expense-list">
-      </div>
-      <div id="total-expense">
-        Total Expense: 400
-      </div>
+      <input id='input1' onChange={handleChange1}/>
+       +
+      <input id='input2' onChange={handleChange2}/> 
+      
+      <p id='result'>{parseInt(firstNum)+parseInt(secondNum)}</p>
     </div>
   )
 }
